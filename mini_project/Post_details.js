ca8a74d30bb5e9a532f7c34d569850eb5b8f4491
DaniilGamers <ss.mihalkivd@gmail.com>
2025-06-30 20:48:35 +0200

    Test full fixation

--- mini_project/Post_details.js
+++ mini_project/Post_details.js
@@ -1,6 +1,6 @@
 //На странице post-details.html:
 // 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
-// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 
 const postId = new URL(location.href).searchParams.get('postId');
 console.log(postId);
